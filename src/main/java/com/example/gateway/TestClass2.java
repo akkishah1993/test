af3cfb46f1b2696dff0f7983efdcea11442f170f
AMujavadiya <AMujavadiya@HIPUNL126579.ad.harman.com>
2021-05-04 12:43:35 +0530

    New test class added

---
+++ src/main/java/com/example/gateway/TestClass2.java
@@ -0,0 +1,32 @@
+package com.example.gateway;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		
+		new TestClass2().test(2);
+	}
+
+	public void test(int param) {
+		switch (param) {
+		  case 0:
+		    doSomething();
+		    break;
+		  default: // default clause should be the last one
+		    error();
+		    break;
+		  case 1:
+		    doSomethingElse();
+		    break;
+		}
+	}
+	public void doSomething() {
+		System.out.println("Do Something");
+	}
+	public void error() {
+		System.out.println("Error");
+	}
+	public void doSomethingElse() {
+		System.out.println("Else");
+	}
+}
